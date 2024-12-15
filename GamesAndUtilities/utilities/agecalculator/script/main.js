7711560119b1e1df40f2f45e46d8c0755d0c1e40
Daniel <156960927+U22099@users.noreply.github.com>
2024-12-15 19:36:29 +0100

    Update main.js

--- GamesAndUtilities/utilities/agecalculator/script/main.js
+++ GamesAndUtilities/utilities/agecalculator/script/main.js
@@ -11,14 +11,14 @@ function calculate(){
     let y = currentDate.getFullYear() - birthdate.getFullYear();
     let m = currentDate.getMonth() - birthdate.getMonth();
     let d = currentDate.getDate() - birthdate.getDate();
-    if(m < 0 || (m = 0 && d < 0)){
-        m += 12;
+    if(m < 0 || (m === 0 && d < 0)){
+        m += m === 0 ? 11 : 12;
         y -= 1;
     }
     if(d < 0){
         let mlim = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
         d += mlim;
-    }y
+    }
     output.innerHTML = "You are "+(y)+" Years "+(m)+" Months and "+d+" Days Old.";
     let speech = new SpeechSynthesisUtterance;
     let voices = window.speechSynthesis.getVoices();
